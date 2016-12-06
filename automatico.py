250f4552ef73e6f4d4b8c5f55e1d8a9af4fb2d61
Aitor Bernal <alu0100830064@ull.edu.es>
2016-12-06 18:40:17 +0000

    1.0

--- automatico.py
+++ automatico.py
@@ -4,12 +4,12 @@ import sys
 def run_command(cmd):
     print 'Running: "%s"' % cmd
     status, text = getstatusoutput(cmd)
-    exit_code = status >> 8 # high byte
-    signal_num = status % 256 # low byte
+    exit_code = status >> 8 
+    signal_num = status % 256 
     print 'Status: x%04x' % status
     print 'Signal: x%02x (%d)' % (signal_num, signal_num)
     print 'Exit  : x%02x (%d)' % (exit_code, exit_code)
-    print 'Core? : %s' % bool(signal_num & (1 << 8)) # high bit
+    print 'Core? : %s' % bool(signal_num & (1 << 8)) 
     print 'Output:'
     print text
     print
